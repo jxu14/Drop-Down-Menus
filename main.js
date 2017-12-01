bfcbf3676039607a24096cccdd26da8377c3a45c
Jeremy <jeremyxu123@gmail.com>
2017-12-01 13:36:32 -0800

    almost done or done probably

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hi");
+$(document).ready(function(){
+  let btn = document.querySelector('button');
+  let content = document.getElementById('myDropdown');
+  let drop = document.getElementById('dropdown');
+
+
+  // btn.addEventListener("click", function() {
+  //   content.classList.toggle("show");
+  // })
+
+  drop.addEventListener("mouseover", function() {
+    content.classList.toggle("show");
+  })
+
+  drop.addEventListener("mouseout", function() {
+    content.classList.remove("show");
+  })
+})
